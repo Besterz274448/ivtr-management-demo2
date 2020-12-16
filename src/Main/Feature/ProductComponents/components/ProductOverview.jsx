0ec53ca26b9d10029a2e5616665552852ec96ee0
watthanai <besterz274448@gmail.com>
2020-12-16 07:49:05 +0700

    update product overview

--- src/Main/Feature/ProductComponents/components/ProductOverview.jsx
+++ src/Main/Feature/ProductComponents/components/ProductOverview.jsx
@@ -7,7 +7,7 @@ export default function ProductOverview({ product_data, closeProductDetail }) {
   return product_data.id === undefined ? (
     <Paper></Paper>
   ) : (
-    <Paper style={{ textAlign: "center" }}>
+    <Paper style={{ textAlign: "center",height:"440px" }}>
         UI for Test Data Only{" "}
         <CloseIcon style={{cursor:"pointer"}}onClick={closeProductDetail}></CloseIcon>
 
